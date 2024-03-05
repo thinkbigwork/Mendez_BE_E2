af503984d6375978eb78bdc6cd058bb6c12ee302
Martín Mendez <70818196+thinkbigwork@users.noreply.github.com>
2024-03-05 20:50:37 -0300

    Update index.js entrega final

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const fs = require('fs')
 
 //Creacion de la clase de productos
-class ProductManager{
-    constructor (){
+class ProductManager {
+    constructor() {
         this.products = []
         this.path = './file.json'
         this.id = 1
     }
-    
+
     // Funcion para leer archivo
     async readDataFromFile() {
         try {
@@ -20,7 +20,7 @@ class ProductManager{
             return JSON.parse(data)
         } catch (err) {
             console.error(`Error: no se encontró el archivo ${this.path}`)
-            throw err;
+            return []
         }
     }
 
@@ -28,24 +28,24 @@ class ProductManager{
     async addProduct(product) {
 
         // Validar campos vacios
-        if(!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock){
+        if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
             console.error('Faltan datos, todos los campos deben estar completos')
-            return 
+            return
         }
         try {
-            if (!this.products.some(p => p.code === product.code)){
-                const newProduct = {id: this.id++, ...product}
+            if (!this.products.some(p => p.code === product.code)) {
+                const newProduct = { id: this.id++, ...product }
                 this.products.push(newProduct)
 
                 await fs.promises.writeFile(this.path, JSON.stringify(this.products), 'utf-8')
                 console.log(`Producto * ${product.title} * agregado correctamente`)
             } else {
                 console.log(`Ya existe un producto con el codigo ${product.code}`);
-              }
+            }
         }
-        catch (err){
+        catch (err) {
             console.error('Error, el archivo no se pudo escribir')
-            throw err
+            return
         }
     }
 
@@ -55,9 +55,9 @@ class ProductManager{
             const allProducts = await this.readDataFromFile()
             return allProducts
         }
-        catch (err){
+        catch (err) {
             console.error('Error, el archivo no se pudo leer')
-            throw err
+            return
         }
     }
 
@@ -68,9 +68,9 @@ class ProductManager{
             const product = allProducts.find(p => p.id === id)
             return product
         }
-        catch (err){
+        catch (err) {
             console.error('No se encontró el producto que buscabas')
-            throw err
+            return
         }
     }
 
@@ -89,7 +89,7 @@ class ProductManager{
             }
         } catch (err) {
             console.error('Error al eliminar producto', err)
-            throw err;
+            return
         }
     }
 }
@@ -99,63 +99,74 @@ class ProductManager{
 const bd = new ProductManager()
 
 
-// Se llama a la funcion para cargar productos
-loadProducts()
-
-
-// Mostrar todos los producto (array completo)
-bd.getProducts().then((products) => {
-    console.log('-----Listado completo de productos-----\n', products)
-})
-
-// Mostrar un producto buscado por id
-bd.getProductsId(2).then((product) => {
-console.log('-----Producto buscado por ID-----\n', product);
-})
-
-// Eliminar un producto por id
-bd.deleteProduct(3).then((product) => {
-console.log('-----Producto eliminado por ID-----\n', product);
-})
-
-
 // Funcion para cargar productos
 async function loadProducts() {
-bd.addProduct({
-    title: 'Manzana',
-    description: 'Manzana roja grande',
-    price: 1500,
-    thumbnail: '../archivo1.jpg',
-    code: codeGenerator(),
-    stock: 50
-})
-bd.addProduct({
-    title: 'Pera',
-    description: 'Pera de Israel',
-    price: 1200,
-    thumbnail: '../archivo2.jpg',
-    code: codeGenerator(),
-    stock: 38
-})
-bd.addProduct({
-    title: 'Uva',
-    description: 'Uva blanca',
-    price: 1800,
-    thumbnail: '../archivo3.jpg',
-    code: codeGenerator(),
-    stock: 457
-})
-bd.addProduct({
-    title: 'Melon',
-    description: 'Melon amarillo',
-    price: 1530,
-    thumbnail: '../archivo4.jpg',
-    code: codeGenerator(),
-    stock: 11
-})
+    await bd.addProduct({
+        title: 'Manzana',
+        description: 'Manzana roja grande',
+        price: 1500,
+        thumbnail: '../archivo1.jpg',
+        code: codeGenerator(),
+        stock: 50
+    })
+    await bd.addProduct({
+        title: 'Pera',
+        description: 'Pera de Israel',
+        price: 1200,
+        thumbnail: '../archivo2.jpg',
+        code: codeGenerator(),
+        stock: 38
+    })
+    await bd.addProduct({
+        title: 'Uva',
+        description: 'Uva blanca',
+        price: 1800,
+        thumbnail: '../archivo3.jpg',
+        code: codeGenerator(),
+        stock: 457
+    })
+    await bd.addProduct({
+        title: 'Melon',
+        description: 'Melon amarillo',
+        price: 1530,
+        thumbnail: '../archivo4.jpg',
+        code: codeGenerator(),
+        stock: 11
+    })
 }
 
-function codeGenerator (){
-let code = Math.random().toString(36).substring(3, 9)
-return code
+function codeGenerator() {
+    let code = Math.random().toString(36).substring(3, 9)
+    return code
 }
+
+
+
+// Programa principal
+const main = async () => {
+    // Mostrar marca de inicio para ayudar a la visualización dentro de la terminal
+    console.log('\n================================================================')
+    console.log('>>>>>                  INICIA EL PROGRAMA                  <<<<<')
+    console.log('================================================================\n')
+
+    // Mostrar array vacio
+    const products = await bd.getProducts()
+    console.log(products)
+
+    // Agregar productos
+    await loadProducts()
+
+    // Mostrar todos los producto (array completo)
+    const products2 = await bd.getProducts()
+    console.log('-----Listado completo de productos-----\n', products2)
+
+    // Mostrar un producto buscado por id
+    const product = await bd.getProductsId(2)
+    console.log('-----Producto buscado por ID-----\n', product)
+
+    // Eliminar un producto por id
+    const deletedProduct = await bd.deleteProduct(3)
+    console.log('-----Producto eliminado por ID-----\n', deletedProduct)
+}
+
+main()
